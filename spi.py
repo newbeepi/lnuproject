80c3d45f1ec2911c3951b76640f8d21637fc8470
dmytro <kozakdmitro15@gmail.com>
2020-04-22 00:28:40 +0300

    next_test

--- spi.py
+++ spi.py
@@ -1,15 +1,17 @@
 import spidev
+from random import randint
 from time import sleep
+commands = ["Left", "Right", "Stop"]
 spi = spidev.spiDev(0,0)
 def sendCommand(command: string):
-	for char im command:
+	for char in command:
 		spi.xfer(char, 1)
 
 
 try:
 	while(True):
-		sendCommand("Left")
-		sleep(2)
+		sendCommand(commands[randint(1, 3)])
+		sleep(3)
 
 except KeyboardInterrupt:
 	spi.close()
